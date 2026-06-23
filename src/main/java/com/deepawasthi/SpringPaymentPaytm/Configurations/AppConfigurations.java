a9267ba7faa42e40abbbeda24c4a7994c57eb837
Deep Awasthi <da.madskull@gmail.com>
2026-06-24 05:09:24 +0530

    Removed Older Files

--- src/main/java/com/deepawasthi/SpringPaymentPaytm/Configurations/AppConfigurations.java
+++
@@ -1,8 +0,0 @@
-package com.deepawasthi.SpringPaymentPaytm.Configurations;
-
-public class AppConfigurations {
-    public static final String MERCHANT_ID = "";
-    public static final String MERCHANT_KEY = "";
-    public static final String WEBSITE = "";
-    public static final String PAYTM_URI = "";
-}
--- src/main/java/com/deepawasthi/SpringPaymentPaytm/Services/PaytmServices.java
+++
@@ -1,8 +0,0 @@
-package com.deepawasthi.SpringPaymentPaytm.Services;
-
-import java.util.Map;
-
-public interface PaytmServices {
-
-    public Map<String, Object> payment(Map<String, Object> data);
-}
